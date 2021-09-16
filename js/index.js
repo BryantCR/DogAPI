ec2ce58cec3b4986096c8f78af24002ac5b15c19
BryantCR <bryancvargas20@gmail.com>
2021-09-16 11:41:39 -0600

    ok

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
 console.log("working");
 
+function fetchDogs( num ){
+    let url = `https://dog.ceo/api/breeds/image/random/${num}`;
+    let settings = {
+        method: "GET"
+    }
+    console.log(1);
+    fetch( url, settings)
+        .then(function(response){
+            console.log(response);
+            console.log(2);
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        
+            for(let i = 0; i < data.message.length; i ++){
+                let result = document.querySelector('.results');
+                result.innerHTML += `
+                <div>
+                <img src="${data.message[i]}" class"size" ></img>
+                </div>
+                `;
+            }
+        });
+        console.log(3);
+}
+
 function grapInfo( event ){
     event.preventDefault();
 
     let currentNumber = event.target.numOfDogs.value;
     console.log(currentNumber);
+    fetchDogs( currentNumber );
 }
 
 let dogForm = document.querySelector("#dogForm");
